ab0487b049b67788ed8b6c3ab229658822c7b503
howardlau1999 <howardlau1999@hotmail.com>
2018-12-04 00:37:23 +0800

    status bar

--- tetris/board.h
+++ tetris/board.h
@@ -2,6 +2,7 @@
 #define __BOARD_H_
 #include <memory>
 #include <vector>
+#include <deque>
 #include <iostream>
 #include <cstdlib>
 #include <SFML/Graphics.hpp>
@@ -12,48 +13,83 @@ private:
 	int rows, cols;
 	std::vector<std::vector<int>> board;
 	std::shared_ptr<Block> falling;
+	int cleared_rows;
 
+	sf::Font font;
+	std::deque<std::shared_ptr<Block>> nextQueue;
 public:
 	const int DEFAULT_ROWS = 22;
 	const int DEFAULT_COLS = 10;
 	const float BLOCK_SIZE = 32.f;
-
+	const float CANVAS_X_OFFSET = 0;
+	const float CANVAS_Y_OFFSET = BLOCK_SIZE * 4;
+	const int maxNexts = 1;
 	TetrisBoard() {
 		new (this) TetrisBoard(DEFAULT_ROWS, DEFAULT_COLS);
 	}
 
-	TetrisBoard(int rows, int cols) : rows(rows), cols(cols),
-		board(std::vector<std::vector<int>>(rows, std::vector<int>(cols, 0))) {}
+	TetrisBoard(int rows, int cols) : rows(rows), cols(cols), cleared_rows(0),
+		board(std::vector<std::vector<int>>(rows, std::vector<int>(cols, 0))) {
+		font.loadFromFile("D:\\arvo.ttf");
+	}
+
+	void drawRectangle(sf::RenderWindow* window, const int c, const int r, const bool absolute = false) {
+		sf::RectangleShape shape(sf::Vector2f(BLOCK_SIZE - 2, BLOCK_SIZE - 2));
+		shape.setPosition(sf::Vector2f(BLOCK_SIZE * c, BLOCK_SIZE * r));
+		if (!absolute)
+			shape.move(sf::Vector2f(CANVAS_X_OFFSET, CANVAS_Y_OFFSET));
+		shape.setFillColor(sf::Color::Green);
+		window->draw(shape);
+	}
+
+	void drawBlock(sf::RenderWindow* window, const Block& block, const int offsetX, const int offsetY, const bool absolute = false) {
+		auto shape = block.getShape();
+		for (int r = 0; r < shape.size(); ++r)
+			for (int c = 0; c < shape[0].size(); ++c) {
+				if (shape[r][c] > 0) {
+					drawRectangle(window, c + offsetX, r + offsetY, absolute);
+				}
+			}
+	}
 
 	void render(sf::RenderWindow* window) {
 		for (int r = 0; r < rows; ++r)
 			for (int c = 0; c < cols; ++c) {
 				if (board[r][c] > 0) {
-					sf::RectangleShape shape(sf::Vector2f(BLOCK_SIZE - 2, BLOCK_SIZE - 2));
-					shape.setPosition(sf::Vector2f(BLOCK_SIZE * c, BLOCK_SIZE * r));
-					shape.setFillColor(sf::Color::Green);
-					window->draw(shape);
+					drawRectangle(window, c, r);
 				}
 			}
-		auto falling_shape = falling->getShape();
-		for (int r = 0; r < falling_shape.size(); ++r) 
-			for (int c = 0; c < falling_shape[0].size(); ++c) {
-				if (falling_shape[r][c] > 0) {
-					sf::RectangleShape shape(sf::Vector2f(BLOCK_SIZE - 2, BLOCK_SIZE - 2));
-					shape.setPosition(sf::Vector2f(BLOCK_SIZE * (c + falling->getX()), BLOCK_SIZE * (r + falling->getY())));
-					shape.setFillColor(sf::Color::Green);
-					window->draw(shape);
-				}
-			}
+		
+		drawBlock(window, *falling, falling->getX(), falling->getY());
+
+		sf::Text scoreText;
+		scoreText.setString("Cleared Rows: " + std::to_string(cleared_rows));
+		scoreText.setFillColor(sf::Color::White);
+		scoreText.setFont(font);
+		window->draw(scoreText);
+
+		sf::Text nextHint;
+		nextHint.setString("NEXT: ");
+		nextHint.setPosition(0, BLOCK_SIZE);
+		nextHint.setFillColor(sf::Color::White);
+		nextHint.setFont(font);
+		window->draw(nextHint);
+
+		drawBlock(window, *nextQueue.front(), 3, 1, true);
 	}
 
 	void generateNewFalling() {
-		falling = BlockFactory::generate(5, 0);
+		while (nextQueue.size() < maxNexts + 1)
+			nextQueue.push_back(BlockFactory::generate(5, 0));
+		falling = nextQueue.front();
+		nextQueue.pop_front();
+		
 	}
 
 	void hardDrop() {}
+
 	void fall() { 
-		if (!checkMove(Block::MOVE_DIR::DOWN)) {
+		if (!checkMove(Block::MOVE_DIR::DOWN, *falling)) {
 			if (falling->getY() == 0) {
 				new (this) TetrisBoard(DEFAULT_ROWS, DEFAULT_COLS);
 				return;
@@ -65,6 +101,7 @@ public:
 			falling->move(Block::MOVE_DIR::DOWN);
 		}
 	}
+
 	void fix() {
 		auto falling_shape = falling->getShape();
 		auto x = falling->getX(); auto y = falling->getY();
@@ -73,16 +110,42 @@ public:
 				if (!board[y + r][x + c])
 					board[y + r][x + c] = falling_shape[r][c];
 	}
-	void move(Block::MOVE_DIR dir) { if (checkMove(dir)) falling->move(dir); }
-	void rotate(Block::ROTATE_DIR dir) { if (checkRotate(dir)) falling->rotate(dir); }
-	bool checkRotate(Block::ROTATE_DIR dir) {
-		auto rotated = Block(*falling);
+
+	void move(Block::MOVE_DIR dir) { 
+		if (checkMove(dir, *falling)) { 
+			falling->move(dir); 
+		} 
+	}
+
+	void rotate(Block::ROTATE_DIR dir) { 
+		if (checkRotate(dir, *falling)) { 
+			falling->rotate(dir); 
+		}  else {
+			auto rotated = std::make_shared<Block>(*falling);
+			rotated->rotate(dir);
+			
+			// If it's too left
+			if (rotated->getX() < 0) {
+				rotated->setX(0);
+			} // Too right
+			else if (rotated->getX() + rotated->getCols() > this->cols) {
+				rotated->setX(this->cols - rotated->getCols() );
+			}
+
+			if (!checkCollision(*rotated)) return;
+
+			falling = rotated;
+		}
+	}
+
+	bool checkRotate(Block::ROTATE_DIR dir, const Block& block) {
+		auto rotated = Block(block);
 		rotated.rotate(dir);
 		return checkBounds(rotated) && checkCollision(rotated);
 	}
 
-	bool checkMove(Block::MOVE_DIR dir) { 
-		auto moved = Block(*falling);
+	bool checkMove(Block::MOVE_DIR dir, const Block& block) { 
+		auto moved = Block(block);
 		moved.move(dir);
 		return checkBounds(moved) && checkCollision(moved);
 	}
@@ -116,6 +179,7 @@ public:
 				++counter;
 			}
 		}
+		cleared_rows += rows - counter;
 		board = _board;
 	}
 };
--- tetris/main.cpp
+++ tetris/main.cpp
@@ -1,48 +1,69 @@
 #include <SFML/Graphics.hpp>
 #include <chrono>
 #include <iostream>
+#include <unordered_set>
 #include "board.h"
 int main() {
-	sf::RenderWindow window(sf::VideoMode(32 * 10, 32 * 22), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(32 * 10, 32 * 26), "SFML works!");
 	sf::RectangleShape shape(sf::Vector2f(32.f, 32.f));
 	TetrisBoard board;
 	board.generateNewFalling();
-	sf::Clock clock, key_delay;
+	sf::Clock clock, key_delay, scan_timer;
 	const sf::Time drop_interval = sf::milliseconds(250);
 	const sf::Time key_interval = sf::milliseconds(100);
-	auto last_key = sf::Keyboard::Left;
+	const sf::Time scan_interval = sf::milliseconds(50);
+
+	auto last_key = sf::Keyboard::Unknown;
+	auto pressed_key = sf::Keyboard::Unknown;
+	static const std::unordered_set<sf::Keyboard::Key> keys = { sf::Keyboard::Left, sf::Keyboard::Right, sf::Keyboard::Down, sf::Keyboard::Up};
 	while (window.isOpen()) {
 		sf::Event event;
+
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-		if (key_delay.getElapsedTime() > key_interval) {
-			key_delay.restart();
 
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-				board.move(Block::MOVE_DIR::LEFT);
-				last_key = sf::Keyboard::Left;
+		if (scan_timer.getElapsedTime() > scan_interval) {
+			bool key_pressed = false;
+			for (auto const& key : keys) {
+				if (sf::Keyboard::isKeyPressed(key)) {
+					pressed_key = key;
+					key_pressed = true;
+					break;
+				}
 			}
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-				board.move(Block::MOVE_DIR::RIGHT);
-				last_key = sf::Keyboard::Right;
-			}
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-				board.fall();
-				last_key = sf::Keyboard::Down;
-				clock.restart();
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-				last_key = sf::Keyboard::Up;
-				board.rotate(Block::ROTATE_DIR::CLOCK);
-				clock.restart();
-			}
+			if (!key_pressed) pressed_key = sf::Keyboard::Unknown;
+			scan_timer.restart();
 		}
+		
+		
+		if (key_delay.getElapsedTime() > key_interval || (last_key != pressed_key && pressed_key != sf::Keyboard::Unknown)) {
+			key_delay.restart();
+			
+			switch (pressed_key) {
+			case sf::Keyboard::Left:
+				board.move(Block::MOVE_DIR::LEFT);
+				break;
+			case sf::Keyboard::Right:
+				board.move(Block::MOVE_DIR::RIGHT);
+				break;
+			case sf::Keyboard::Down:
+				board.fall();
+				clock.restart();
+				break;
+			case sf::Keyboard::Up:
+				if (pressed_key == last_key) break;
+				board.rotate(Block::ROTATE_DIR::CLOCK);
+				break;
+			default:
+				break;
+			}
+
+			last_key = pressed_key;
+		}
+
 		if (clock.getElapsedTime() > drop_interval) {
-			last_key = sf::Keyboard::Down;
 			clock.restart();
 			board.fall();
 		}
